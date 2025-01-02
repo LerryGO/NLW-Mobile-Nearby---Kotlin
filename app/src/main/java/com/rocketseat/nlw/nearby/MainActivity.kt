2e01ef3c442428604e0eae58ed22c52adbd0709a
Lerry Augusto <lerry.pirani@gmail.com>
2025-01-01 22:39:06 -0300

    markets and categories from API, marks in googlemaps done

--- app/src/main/java/com/rocketseat/nlw/nearby/MainActivity.kt
+++ app/src/main/java/com/rocketseat/nlw/nearby/MainActivity.kt
@@ -4,16 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.toRoute
 import com.rocketseat.nlw.nearby.data.model.Market
 import com.rocketseat.nlw.nearby.ui.screen.HomeScreen
+import com.rocketseat.nlw.nearby.ui.screen.HomeViewModel
 import com.rocketseat.nlw.nearby.ui.screen.MarketDetailsScreen
 import com.rocketseat.nlw.nearby.ui.screen.SplashScreen
 import com.rocketseat.nlw.nearby.ui.screen.WelcomeScreen
@@ -21,6 +22,8 @@ import com.rocketseat.nlw.nearby.ui.screen.route.Home
 import com.rocketseat.nlw.nearby.ui.screen.route.Splash
 import com.rocketseat.nlw.nearby.ui.screen.route.Welcome
 import com.rocketseat.nlw.nearby.ui.theme.NearbyTheme
+import androidx.compose.runtime.getValue
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,6 +32,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             NearbyTheme {
                 val navController = rememberNavController()
+
+                val homeViewModel by viewModels<HomeViewModel>()
+                val homeUiState by homeViewModel.uiState.collectAsStateWithLifecycle()
+
                 NavHost(
                     navController = navController,
                     startDestination = Splash
@@ -49,7 +56,9 @@ class MainActivity : ComponentActivity() {
                     composable<Home> {
                         HomeScreen(onNavigateToMarketDetails = { selectedMarket ->
                             navController.navigate(selectedMarket)
-                        })
+                        },
+                            uiState = homeUiState,
+                            onEvent = homeViewModel::onEvent )
                     }
                     composable<Market> {
                         val selectedMarket = it.toRoute<Market>()
